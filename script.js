4be7c026d105bd906dbfdf57f9c0ebcb9f63e56b
Basil T j=Joy <basiltjoy96@gmail.com>
2022-03-13 21:47:56 +0530

    initiate

--- script.js
+++ script.js
@@ -19,3 +19,22 @@ function displayValue(data) {
     Location wind speed:  ${wind_speed}   `
     document.querySelector(".result").innerHTML = weather_data
 }
+
+/*{"coord":{"lon":77.2167,"lat":28.6667},
+"weather":[{"id":711,"main":"Smoke","description":"smoke","icon":"50d"}],
+"base":"stations",
+"main":{"temp":25.05,
+"feels_like":24.76,
+"temp_min":25.05,
+"temp_max":25.05,
+"pressure":1015,
+"humidity":44},
+"visibility":1500,
+"wind":{"speed":2.57,"deg":240},
+"clouds":{"all":6},
+"dt":1636527211,
+"sys":{"type":1,
+"id":9165,"country":"IN",
+"sunrise":1636506594,"sunset":1636545617},
+"timezone":19800,"id":1273294,
+"name":"Delhi","cod":200} */
